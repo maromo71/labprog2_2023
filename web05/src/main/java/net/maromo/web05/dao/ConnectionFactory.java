3280ba5bd76468e4eceba518ec33d87ebb33d9cd
maromo71 <professormoraes@gmail.com>
2023-06-23 15:38:33 -0300

    commit junho 23

---
+++ web05/src/main/java/net/maromo/web05/dao/ConnectionFactory.java
@@ -0,0 +1,21 @@
+package net.maromo.web05.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionFactory {
+    public Connection getConnection() throws ClassNotFoundException {
+        final Connection conn;
+
+        Class.forName("com.mysql.jdbc.Driver");
+        try {
+            conn = DriverManager.getConnection("jdbc:mysql://localhost/times", "root", "1234");
+
+        } catch (SQLException ex) {
+            throw new RuntimeException(ex);
+        }
+        return conn;
+    }
+
+}
